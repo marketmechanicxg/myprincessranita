15ea95689a864483e857362574df95230b9ace41
marketmechanicxg <grizzyxixi@gmail.com>
2026-05-24 18:48:49 +0700

    Update audio-system.js

--- js/audio-system.js
+++ js/audio-system.js
@@ -14,11 +14,11 @@
 const playlist = [
   {
     id:        'song1',
-    title:     'Sampai Jadi Debu',
-    artist:    'Banda Neira',
+    title:     'One Call Away',
+    artist:    'Charlie Puth',
     file:      'music/song1.mp3',
     cover:     'assets/music-cover/song1.jpeg',
-    startTime: 0,       // seconds — where playback begins
+    startTime: 130,       // seconds — where playback begins
     endTime:   null,    // seconds — where it ends/loops (null = full track)
     loopStart: null,    // loop rewind point (null = startTime)
     volume:    0.75,
